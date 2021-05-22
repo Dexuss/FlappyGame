d4d1f419399e5c62f04b417e44a582421e455c8d
Konrad <dexus1992@gmail.com>
2021-05-22 18:51:31 +0200

    Pipes data edit

--- FlappyGame/Assets/_Scripts/Game.Environment/SinglePipes.cs
+++ FlappyGame/Assets/_Scripts/Game.Environment/SinglePipes.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Game.Data;
+using UnityEngine;
+using Zenject;
 
 public class SinglePipes : MonoBehaviour, ISinglePipes
 {
@@ -11,10 +13,26 @@ public class SinglePipes : MonoBehaviour, ISinglePipes
     [SerializeField]
     private SpriteRenderer bottomPipeSprite;
 
-    //private PipesData currentPipesData;
+    private PipesData currentPipesData;
 
     public Transform GetPipesTransform()
     {
         return pipesTransform;
+    }
+
+    public void InitialPipesData(PipesData currentPipesData)
+    {
+        var pipeSprite = currentPipesData.pipeSprite; ;
+        var pipeColor = currentPipesData.pipesColor;
+        topPipeSprite.sprite = pipeSprite;
+        bottomPipeSprite.sprite = pipeSprite;
+        topPipeSprite.color = pipeColor;
+        bottomPipeSprite.color = pipeColor;
+        this.currentPipesData = currentPipesData;
+    }
+
+    public int GetRequiredPoints()
+    {
+        return currentPipesData.requiredPoints;
     }
 }
